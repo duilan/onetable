7f75253736a11cd3fe12a46cecca397251cb99bd
Adrian Cortes <1DwylaN9@gmail.com>
2016-11-24 04:01:09 -0600

    image intervention
    Se integro image intervention para redimensionar las imágenes
    (logotipos, fotos)

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
          */
         Barryvdh\Debugbar\ServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
+
         //
 
         /*
@@ -230,6 +232,7 @@ return [
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
